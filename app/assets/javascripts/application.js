c29f9a6a01f220dbf9929f24103a8aec68c172bb
Yuttanant Suwansiri <ysuwansiri@yahoo.com>
2013-11-20 23:27:23 +0700

    test calendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,65 +12,65 @@
 //var test_var = "testing var";
 $("#schedule_date").datepicker(); 
 
-//$(document).ready(function() {   
-//
-//        var date = new Date();
-//        var d = date.getDate();
-//        var m = date.getMonth();
-//        var y = date.getFullYear();
-//       
-//
-//        $('#calendar').fullCalendar({
-//
-//                header: {
-//                        left: 'prev,next today',
-//                        center: 'title',
-//                        right: 'month,basicWeek,basicDay'
-//                },
-//                editable: false,
-//                events: [
-//                        {
-//                                title: test_var,
-//                                start: new Date(y, m, 3)
-//                        },
-//                        {
-//                                id: 999,
-//                                title: 'Repeating Event',
-//                                start: new Date(y, m, d-3, 16, 0),
-//                                allDay: false
-//                        },
-//                        {
-//                                id: 999,
-//                                title: 'Repeating Event',
-//                                start: new Date(y, m, d+4, 16, 0),
-//                                allDay: false
-//                        },
-//                        {
-//                                title: 'Meeting',
-//                                start: new Date(y, m, d, 10, 30),
-//                                allDay: false
-//                        },
-//                        {
-//                                title: 'Lunch',
-//                                start: new Date(y, m, d, 12, 0),
-//                                end: new Date(y, m, d, 14, 0),
-//                                allDay: false
-//                        },
-//                        {
-//                                title: 'Birthday Party',
-//                                start: new Date(y, m, d+1, 19, 0),
-//                                end: new Date(y, m, d+1, 22, 30),
-//                                allDay: false
-//                        },
-//                        {
-//                                title: 'Click for Google',
-//                                start: new Date(y, m, 28),
-//                                end: new Date(y, m, 29),
-//                                url: 'http://google.com/'
-//                        }
-//                ]
-//        });
-//
-//
-//
-//});
+$(document).ready(function() {   
+
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+       
+
+        $('#calendar').fullCalendar({
+
+                header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,basicWeek,basicDay'
+                },
+                editable: false,
+                events: [
+                        {
+                                title: test_var,
+                                start: new Date(y, m, 3)
+                        },
+                        {
+                                id: 999,
+                                title: 'Repeating Event',
+                                start: new Date(y, m, d-3, 16, 0),
+                                allDay: false
+                        },
+                        {
+                                id: 999,
+                                title: 'Repeating Event',
+                                start: new Date(y, m, d+4, 16, 0),
+                                allDay: false
+                        },
+                        {
+                                title: 'Meeting',
+                                start: new Date(y, m, d, 10, 30),
+                                allDay: false
+                        },
+                        {
+                                title: 'Lunch',
+                                start: new Date(y, m, d, 12, 0),
+                                end: new Date(y, m, d, 14, 0),
+                                allDay: false
+                        },
+                        {
+                                title: 'Birthday Party',
+                                start: new Date(y, m, d+1, 19, 0),
+                                end: new Date(y, m, d+1, 22, 30),
+                                allDay: false
+                        },
+                        {
+                                title: 'Click for Google',
+                                start: new Date(y, m, 28),
+                                end: new Date(y, m, 29),
+                                url: 'http://google.com/'
+                        }
+                ]
+        });
+
+
+
+});
