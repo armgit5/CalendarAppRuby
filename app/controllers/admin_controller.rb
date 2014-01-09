a6eadbe6c888a610530300efa189793d7def0172
Yuttanant Suwansiri <ysuwansiri@yahoo.com>
2014-01-09 11:20:50 +0700

    changes on color, wordings, edit and delete location page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
+  
+  http_basic_authenticate_with :name => "admin", :password => "nilp3ter"
+  
   def index
     @companies = Company.order('name ASC').paginate(:per_page => 25, :page => params[:page])
   end
