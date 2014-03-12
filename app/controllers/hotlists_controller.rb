da7faa4dc335792a0c5340891990c3e58510b4d3
Yuttanant Suwansiri <arm@Yuttanants-MacBook-Pro.local>
2014-03-12 17:10:58 +0700

    add hotlists page and change css style

--- app/controllers/hotlists_controller.rb
+++ app/controllers/hotlists_controller.rb
@@ -1,6 +1,8 @@
 class HotlistsController < ApplicationController
   def index
-    
+    time_ref = Time.now - 60 * 1
+    @pendings = Pending.where(:status_id => 1)
+    @orders = Pending.where("updated_at > ? and status_id = 2", "#{time_ref}")
   end
 
 end
--- app/controllers/pending_controller.rb
+++ app/controllers/pending_controller.rb
@@ -1,7 +1,33 @@
+#class PendingController < ApplicationController
+#  def index
+#    @pendings = Pending.all
+#    
+#  end
+#  
+#  def add_pending
+#    @sale = Sale.all
+#    @companies = Company.all
+#    @statuses = Status.all
+#  end
+#  
+#  def create
+#    @pendings = Pending.create!(params[:pending])
+#    flash[:notice] = "#{@pendings.project} was successfully created."
+#    redirect_to(:action => "index")
+#  end
+#  
+#  def edit
+#    @pendings = Pending.find(params[:id])
+#    @sales = Sale.all
+#    @companies = Company.all
+#  end
+#  
+# 
+#
+#end
 class PendingController < ApplicationController
   def index
     @pendings = Pending.all
-    
   end
   
   def add_pending
@@ -16,12 +42,30 @@ class PendingController < ApplicationController
     redirect_to(:action => "index")
   end
   
-  def edit
+  def edit_pending
     @pending = Pending.find(params[:id])
     @sale = Sale.all
     @companies = Company.all
+    @statuses = Status.all
   end
   
- 
+  def update
+    @pending = Pending.find(params[:id])
+    @pending.update_attributes!(params[:pending])
+     
+    flash[:notice] = "#{@pending.project} was successfully updated."
+    redirect_to(:action => "index")
+  end
+  
+  def delete
+    @pending = Pending.find(params[:id])
+  end
+  
+  def destroy
+    @pending = Pending.find(params[:id])
+    @pending.destroy
+    flash[:notice] = "#{@pending.project} was successfully deleted."
+    redirect_to(:action => "index")
+  end
 
 end
