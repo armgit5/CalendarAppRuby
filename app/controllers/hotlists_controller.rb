7906d28aefcb17ac78646bd71d7014a43546aa7c
Yuttanant Suwansiri <arm@Yuttanants-MacBook-Pro.local>
2014-03-24 18:07:20 +0700

    added moving text hotlists:

--- app/controllers/hotlists_controller.rb
+++ app/controllers/hotlists_controller.rb
@@ -1,6 +1,6 @@
 class HotlistsController < ApplicationController
   def index
-    time_ref = Time.now - 60 * 1
+    time_ref = Time.now.utc - 60 * 1
     @pendings = Pending.where(:status_id => 1)
     @orders = Pending.where("updated_at > ? and status_id = 2", "#{time_ref}")
   end
