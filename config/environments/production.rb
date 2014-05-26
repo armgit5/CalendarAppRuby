ff9c34a75c31c4448ce33a40732bee76f04c6e64
Yuttanant Suwansiri <arm@Yuttanants-MacBook-Pro.local>
2014-05-26 17:32:46 +0700

    heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,5 +57,6 @@ NilpeterWeb::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.initialize_on_precompile=false
   
 end
