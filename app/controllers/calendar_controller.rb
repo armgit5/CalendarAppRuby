3c2d9d08ad13da8062a679711d9ebd3a7b9c55bc
Arm <arm@Yuttanants-MacBook-Pro.local>
2015-06-23 11:50:54 +0700

    fixed update and search v2

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,16 +3,16 @@ class CalendarController < ApplicationController
   before_filter :authorize
   
   def index
-    @time = params[:time_id]
+#    @time = params[:time_id]
     @schedule = Schedule.search(params[:search])
     @holidays = Holiday.all
-    @last_id = @schedule.last.id
+#    @last_id = @schedule.last.id
 #    created_time = @schedule.find(@last_id).created_at
 #    @test = Schedule.where('created_at > ?', created_time)
-    if params[:schedule_id]  
-      created_time = @schedule.find(params[:schedule_id]).created_at
-      @test = Schedule.where('created_at > ?', created_time)
-    end
+#    if params[:schedule_id]  
+#      created_time = @schedule.find(params[:schedule_id]).created_at
+#      @test = Schedule.where('created_at > ?', created_time)
+#    end
     
     
   end
