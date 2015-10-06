826570d2a85b43d7a0b2e600f97e636c52d4c06c
Arm Suwansiri <ysuwansiri@yahoo.com>
2015-10-06 11:39:13 +0700

    api username and password

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,8 +2,6 @@ class Api::ProductsController < Api::ApiController
 
       def index
         render json: Product.all
-        Rails.logger.info "Current user: #{current_user.inspect}"
-        
       end
 
 end
