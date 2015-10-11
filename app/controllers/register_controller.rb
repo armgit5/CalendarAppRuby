88be33e8fd0601d8a70a67304528151ffa847baf
Arm Suwansiri <ysuwansiri@yahoo.com>
2015-10-11 21:39:18 +0700

    fix edit delete bugs with user id and delete page color to black

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -4,22 +4,22 @@ class RegisterController < ApplicationController
 #    @calendar = Schedule.find(id) # look up movie by unique ID
 #    # will render app/views/movies/show.<extension> by default
 #  end
-  
+
 #  http_basic_authenticate_with :name => "sales", :password => "npasia"
-  
+
   before_filter :authorize
-  
+
   def schedule
     @sale = Sale.all
     @companies = Company.order("name asc")
     @locations = Location.order("name asc")
-    
+
     @nilpeter_products = Product.where(:type_id => 1)
     @oem1 = Product.where(:type_id => 2).limit(6)
     @oem2 = Product.where(:type_id => 2).last(5)
     @all_days = [1]
   end
-  
+
   def index
     params[:sort] ||= "date"
     params[:direction] ||= "desc"
@@ -27,19 +27,19 @@ class RegisterController < ApplicationController
 #    if current_user.role_id != 3
 #      @schedule = Schedule.where(sale_id: User.find(current_user.id).sale_id)
 #      .order(params[:sort] + " " + params[:direction]).search(params[:search])
-#      .paginate(:per_page => 25, :page => params[:page])  
-#    else 
+#      .paginate(:per_page => 25, :page => params[:page])
+#    else
     @schedule = Schedule.search(params[:search])
     .order(params[:sort] + " " + params[:direction])
     .paginate(:per_page => 25, :page => params[:page])
 #    end
     @nilpeter_products = Product.where(:type_id => 1)
     @meech_products = Product.where(:type_id => 2)
-    
+
     @test =  current_user
-    
+
   end
-  
+
   def export_csv
     schedules = Schedule.all
     csvdata = CSV.generate do |csv|
@@ -48,7 +48,7 @@ class RegisterController < ApplicationController
       schedules.each do |s|
         if s.location.nil?
           csv << [s.date, s.company.name, s.sale.name, s.project]
-          else 
+          else
           csv << [s.date, s.company.name, s.location.name, s.sale.name, s.project]
         end
       end
@@ -56,25 +56,25 @@ class RegisterController < ApplicationController
     send_data(csvdata, :type => 'text/csv', :filename => 'saleapp_export.csv')
   end
 
-  def show 
+  def show
     @schedule = Schedule.find(params[:id])
   end
-#  
+#
   def edit
     @schedule = Schedule.find(params[:id])
-    if current_user.sale_id != @schedule.sale_id
+    if current_user.id != @schedule.user_id
       redirect_to(:action => "index")
     end
-    
+
     @sale = Sale.all
     @companies = Company.all
     @locations = Location.all
-    
+
     @nilpeter_products = Product.where(:type_id => 1)
     @oem1 = Product.where(:type_id => 2).limit(6)
     @oem2 = Product.where(:type_id => 2).last(5)
   end
-  
+
   def update
     @schedule = Schedule.find(params[:id])
     @schedule.update_attributes!(params[:schedule])
@@ -83,21 +83,21 @@ class RegisterController < ApplicationController
 #    redirect_to(:action => "show", :id => @schedule.id)
     redirect_to(:action => "index")
   end
-  
+
   def delete
     @schedule = Schedule.find(params[:id])
-    if current_user.sale_id != @schedule.sale_id
+    if current_user.id != @schedule.user_id
       redirect_to(:action => "index")
     end
   end
-  
+
   def destroy
     @schedule = Schedule.find(params[:id])
     @schedule.destroy
     flash[:notice] = "#{@schedule.project} was successfully deleted."
     redirect_to(:action => "index")
   end
-  
+
   def create
     @schedule = Schedule.create!(params[:schedule])
 
@@ -105,24 +105,24 @@ class RegisterController < ApplicationController
     flash[:notice] = "#{@schedule.project} was successfully created."
     redirect_to(:controller => "calendar", :action => "index")
   end
-  
+
   def calendar
     @schedule = Schedule.search(params[:search])
-#    old_number = Schedule.count 
-    
+#    old_number = Schedule.count
+
 #    while true do
 #      sleep 3
-#      num_data = Schedule.count 
-#      if num_data != old_number 
+#      num_data = Schedule.count
+#      if num_data != old_number
 #        redirect_to(:action => "calendar")
 #      end
-#    end 
+#    end
   end
-  
+
   def create_new
     @company = Company.create!(params[:company])
     flash[:notice] = "#{@company.name} was successfully created."
     redirect_to(:action => "schedule")
   end
-  
+
 end
