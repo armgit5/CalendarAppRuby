3adcfaca80fa009d8bc558b75fbc5346ea9d45dc
Arm Suwansiri <ysuwansiri@yahoo.com>
2015-10-27 21:09:04 +0700

    change aws key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
 #    :provider               => 'AWS',
-#    :aws_access_key_id      => 'AKIAIQHRF3LQ5RRMHIDQ',
-#    :aws_secret_access_key  => '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'
+#    :aws_access_key_id      => 'Key',
+#    :aws_secret_access_key  => 'Key'
   }
   config.fog_directory  = 'test-nilpeter' # bucket name
 end
