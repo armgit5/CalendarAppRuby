92c76ec27931414bd1b377426547377c03411066
Arm Suwansiri <ysuwansiri@yahoo.com>
2015-12-24 17:19:27 +0700

    update date time in current looking at  month

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,6 +3,11 @@ class CalendarController < ApplicationController
   before_filter :authorize
 
   def index
+    if params[:month] == nil
+      Rails.logger.info "None Month = #{params[:month]}"
+      params[:month] = Time.now.month
+    end
+    Rails.logger.info "Month = #{params[:month]}"
     @schedule = Schedule.where('extract(month from date) = ?', params[:month])
     @holidays = Holiday.all
   end
