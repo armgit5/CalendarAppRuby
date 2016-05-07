fa14b2f3e764ed651ed78588da0e79ad11bc976c
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-05-07 18:57:22 +0700

    login with out @nilpeter.com

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    para = ""
+    if !params[:email].include? "@"
+      para = params[:email] + "@nilpeter.com"
+    end
+    Rails.logger.info "API authentication: #{para}"
+    user = User.find_by_email(para)
+
     if user && user.authenticate(params[:password])
 #      session[:user_id] = user.id
       if params[:remember_me]
        cookies.permanent[:auth_token] = user.auth_token
       else
-       cookies[:auth_token] = user.auth_token  
+       cookies[:auth_token] = user.auth_token
       end
       if user.role_id == 3
         redirect_to(:controller => "admin", notice: "Logged in!")
@@ -20,7 +26,7 @@ class SessionsController < ApplicationController
       redirect_to login_url, notice: "Email or password is invalid."
     end
   end
-  
+
   def destroy
 #    session[:user_id] = nil
     cookies.delete(:auth_token)
