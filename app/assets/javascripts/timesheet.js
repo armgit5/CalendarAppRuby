7284c3826aa57fc6e1617e811b1e77ce0eb1b271
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-07-04 01:06:05 +0700

    testing angular with timesheet

---
+++ app/assets/javascripts/timesheet.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = angular.module('Timesheet', [])
+app.controller('TimesheetCtrl', ['$scope', '$http', function($scope, $http) {
+  // // console.log($scope.timeSheet.jobNo);
+  // $scope.timeSheet.jobNo = "fdsa";
+
+  $scope.deselect = function() {
+    console.log($scope.jobno);
+    console.log('test');
+    $scope.jobno = "test123";
+  }
+}]);
