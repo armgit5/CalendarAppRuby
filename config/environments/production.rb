7d689e22313639a363b4c97c493fa7405f6d3d13
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-07-04 01:12:50 +0700

    add timesheet.js to asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ NilpeterWeb::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false  
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -43,7 +43,7 @@ NilpeterWeb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += %w( jquery.datetimepicker.css jquery.datetimepicker.js select2.css select2.js schedule2.js)
+   config.assets.precompile += %w( jquery.datetimepicker.css jquery.datetimepicker.js select2.css select2.js schedule2.js timesheet.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -58,5 +58,5 @@ NilpeterWeb::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 #  config.assets.initialize_on_precompile=false
-  
+
 end
