63d7695c4cbce57f8be14300f9ab817104a58697
Arm Suwansiri <ysuwansiri@yahoo.com>
2016-07-12 15:42:29 +0700

    can edit timesheet from calebdar page

--- app/controllers/ioscalendar_controller.rb
+++ app/controllers/ioscalendar_controller.rb
@@ -2,17 +2,17 @@ class IoscalendarController < ActionController::Base
 
   require 'json'
 
+  def current_user
+    @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
+  end
+
   def index
-    # if params[:month] == nil
-    #   Rails.logger.info "None Month = #{params[:month]}"
-    #   params[:month] = Time.now.month
-    # end
-    # @schedule = Schedule.where('extract(month from date) = ? OR extract(month from end_date) = ?', params[:month], params[:month])
     @schedule = Schedule.all
     @holidays = Holiday.all
   end
 
   def create_timesheet
+
     Rails.logger.info "timesheet data controller value #{params[:timesheet_nil_sig]}"
     @timesheet = Timesheet.new
     @timesheet.user_id = params[:user_id]
@@ -26,14 +26,19 @@ class IoscalendarController < ActionController::Base
     @schedule.timesheet_id = @timesheet.id
     @schedule.update_attributes!(params[:timesheet_id])
 
-    # flash[:notice] = "#{@timesheet.job_num} was successfully created."
     redirect_to(:controller => "register", :action => "index")
   end
 
   def timesheet
+
     id = params[:id]
     @schedule_id = id
     @schedule = Schedule.find(id)
+
+    if current_user.id != @schedule.user_id and current_user.role_id != 3
+      redirect_to(:controller => "calendar", :action => "index")
+    end
+
     @current_user_id = @schedule.user_id
     @engineers = ""
     for user in @schedule.users.reverse
@@ -49,23 +54,13 @@ class IoscalendarController < ActionController::Base
   end
 
   def edit_timesheet
-    # id = params[:id]
-    # @schedule_id = id
-    # @schedule = Schedule.find(id)
-    # @current_user_id = @schedule.user_id
-    # @engineers = ""
-    # for user in @schedule.users.reverse
-    #   @engineers = user.email.split("@")[0].upcase + ", " + @engineers
-    # end
-    # @engineers = @engineers.chop.chop
-    #
-    # @product_types = ""
-    # for product in @schedule.products.reverse
-    #   @product_types = product.name.upcase + ", " + @product_types
-    # end
-    # @product_types = @product_types.chop.chop
 
-    # @timesheet = Timesheet.where(schedule_id: params[:id]).last
+    @schedule = Schedule.where(timesheet_id: params[:id])[0]
+
+    if current_user.id != @schedule.user_id and current_user.role_id != 3
+      redirect_to(:controller => "register", :action => "index")
+    end
+
     @timesheet = Timesheet.find(params[:id])
     @timesheet_id = params[:id]
     @timesheet_data = @timesheet.data
@@ -74,6 +69,8 @@ class IoscalendarController < ActionController::Base
   end
 
   def update_timesheet
+
+    @schedule = Schedule.find(id)
     @timesheet = Timesheet.find(params[:id])
     @timesheet.data = params[:data]
     @timesheet.nil_sig = params[:nil_sig]
--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -107,6 +107,7 @@ class RegisterController < ApplicationController
 #
   def edit
     @schedule = Schedule.find(params[:id])
+    
     if current_user.id != @schedule.user_id and current_user.role_id != 3
       redirect_to(:controller => "calendar", :action => "index")
     end
