7d77a2825bceee19f5abf8818a40b2e332dc17c7
Arm Suwansiri <ysuwansiri@yahoo.com>
2017-04-21 21:54:16 +0700

    updated

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-# ruby "2.1.0"
+# ruby "2.4.0"
 gem 'rails', '3.2.0'
 
 # Bundle edge Rails instead:
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 #  config.assets.initialize_on_precompile = false
 
-private 
+private
 
   def current_user
 #    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
+  # Rails.logger.info User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
+    # @current_user = User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+    @current_user ||= User.where("auth_token =?", cookies[:auth_token]).first if cookies[:auth_token]
   end
   helper_method :current_user
-  
+
   def authorize
     redirect_to login_url, notice: "Not authorized" if current_user.nil?
   end
-  
+
   def authorize_admin
     redirect_to login_url, notice: "Please login as admin" if current_user.role_id != 3
   end
